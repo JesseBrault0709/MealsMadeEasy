b36f211392b15d4a8dfa8ac6af41a9ea747406f2
Sandra13041009 <80854643+Sandra13041009@users.noreply.github.com>
2021-06-16 08:56:03 +0200

    implemented React and Axios

---
+++ meal_planning/src/App.js
@@ -0,0 +1,12 @@
+import Welcome from "./screens/Welcome";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ meal_planning/src/screens/Welcome.js
@@ -0,0 +1,6 @@
+import React from "react";
+import axios from "axios";
+
+export default function Welcome() {
+  return <div>Hi</div>;
+}
