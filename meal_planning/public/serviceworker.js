0479ce885591ce56dfcd3180e9279a27276e5b69
Sandra13041009 <80854643+Sandra13041009@users.noreply.github.com>
2021-06-20 00:04:04 +0200

    submitted manifest.json

---
+++ meal_planning/public/serviceworker.js
@@ -0,0 +1,36 @@
+const CACHE = "base";
+const urlsToCache = ["index.html", "offline.html"];
+
+const self = this;
+
+//Install ServiceWorker
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE)).then((cache) => {
+    return cache.addAll(urlsToCache);
+  });
+});
+//Requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(() => {
+        return fetch(event.request);
+      })
+      .catch(() => caches.match("offline.html"))
+  );
+});
+//ServiceWorker Activation
+self.addEventListener("activate", (event) => {
+  const newCache = [];
+  newCache.push(CACHE);
+  event.waitUntil(caches.keys()).then((cacheNames) =>
+    Promise.all(
+      cacheNames.map((cacheName) => {
+        if (!newCache.includes(cacheName)) {
+          return caches.delete(cacheName);
+        }
+      })
+    )
+  );
+});
