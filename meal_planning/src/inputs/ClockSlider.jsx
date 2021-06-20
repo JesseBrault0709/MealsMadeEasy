bf21b90a87a6bbbb196859404d55b95c62e2e3e4
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-06-20 16:53:56 -0500

    refactor and better documentation

--- meal_planning/src/inputs/ClockSlider.jsx
+++ meal_planning/src/inputs/ClockSlider.jsx
@@ -70,10 +70,43 @@ const timingOptions = {
 }
 
 /**
+ * Returns a function that, when called:
+ *  1. Checks if the checkPredicate returns true, and if so:
+ *  2. Runs the specified animations, and
+ *  3. Runs the given onFinished callback when all animations are finished.
+ * 
+ * If the checkPredicate returns false, it is a no-op.
+ * 
+ * @param {() => boolean} checkPredicate 
+ * @param {() => Animation[]} getAnimations 
+ * @param {() => void} onFinished 
+ */
+function getRotation(checkPredicate, getAnimations, onFinished) {
+
+    return () => {
+        if (checkPredicate()) {
+            Promise.all(
+                getAnimations().map(animation => animation.finished)
+            )
+                .then(onFinished)
+                .catch(console.log)
+        }   
+    }
+
+}
+
+
+/**
  * Props:
  *  * options: string[]: an array of string options containing at least one element
  *  * initialOption: number, default = 0: the index of the initially selected option
  *  * onChange: (selectedIndex: number) => void: a callback for when the selected option changes
+ * 
+ * @param {{
+ *  options: string[],
+ *  initialOption?: number,
+ *  onChange?: (selectedIndex: number) => void
+ * }} props
  */
 export function ClockSlider(props) {
 
@@ -92,57 +125,44 @@ export function ClockSlider(props) {
     const rightRef = useRef()
     const rightHiddenRef = useRef()
 
+    /**
+     * @param {number} nextCenter 
+     */
     function runOnChangeCallback(nextCenter) {
         if (props.onChange !== undefined && props.onChange !== null) {
             props.onChange(nextCenter)
         }
     }
 
-    function clockwise() {
-
-        // check if we are greater than 0 first before rotating
-        if (currentCenter > 0) {
-            const leftHiddenAnimation = leftHiddenRef.current.animate(leftHiddenToLeft, timingOptions)
-            const leftAnimation = leftRef.current.animate(leftToCenter, timingOptions)
-            const centerAnimation = centerRef.current.animate(centerToRight, timingOptions)
-            const rightAnimation = rightRef.current.animate(rightToRightHidden, timingOptions)
-
-            Promise.all([
-                leftHiddenAnimation.finished,
-                leftAnimation.finished,
-                centerAnimation.finished,
-                rightAnimation.finished
-            ]).then(() => {
-                const nextCenter = currentCenter - 1 // guaranteed to be greater than or equal to zero
-                setCurrentCenter(nextCenter)
-                runOnChangeCallback(nextCenter)
-            })
+    const clockwise = getRotation(
+        () => currentCenter > 0,
+        () => [
+            leftHiddenRef.current.animate(leftHiddenToLeft, timingOptions),
+            leftRef.current.animate(leftToCenter, timingOptions),
+            centerRef.current.animate(centerToRight, timingOptions),
+            rightRef.current.animate(rightToRightHidden, timingOptions)
+        ],
+        () => {
+            const nextCenter = currentCenter - 1 // guaranteed to be greater than or equal to zero
+            setCurrentCenter(nextCenter)
+            runOnChangeCallback(nextCenter)
         }
+    )
 
-    }
-
-    function counterClockwise() {
-
-        // check that we aren't centered on the last element already
-        if (currentCenter < options.length - 1) {
-            const rightHiddenAnimation = rightHiddenRef.current.animate(rightHiddenToRight, timingOptions)
-            const rightAnimation = rightRef.current.animate(rightToCenter, timingOptions)
-            const centerAnimation = centerRef.current.animate(centerToLeft, timingOptions)
-            const leftAnimation = leftRef.current.animate(leftToLeftHidden, timingOptions)
-
-            Promise.all([
-                rightHiddenAnimation.finished,
-                rightAnimation.finished,
-                centerAnimation.finished,
-                leftAnimation.finished
-            ]).then(() => {
-                const nextCenter = currentCenter + 1 // guaranteed to be less than or equal to index of last element
-                setCurrentCenter(nextCenter)
-                runOnChangeCallback(nextCenter)
-            })
+    const counterClockwise = getRotation(
+        () => currentCenter < options.length - 1,
+        () => [
+            rightHiddenRef.current.animate(rightHiddenToRight, timingOptions),
+            rightRef.current.animate(rightToCenter, timingOptions),
+            centerRef.current.animate(centerToLeft, timingOptions),
+            leftRef.current.animate(leftToLeftHidden, timingOptions)
+        ],
+        () => {
+            const nextCenter = currentCenter + 1 // guaranteed to be less than or equal to index of last element
+            setCurrentCenter(nextCenter)
+            runOnChangeCallback(nextCenter)
         }
-
-    }
+    )
 
     return <>
         <div className="clock-slider">
