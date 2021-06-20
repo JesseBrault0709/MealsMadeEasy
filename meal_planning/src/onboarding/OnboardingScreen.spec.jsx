35d86e9e57a88ce9f647b8b942bbba42a4f16c90
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-06-20 14:54:53 -0500

    added OnboardingSreen test

---
+++ meal_planning/src/onboarding/OnboardingScreen.spec.jsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react'
+import { OnboardingScreen } from './OnboardingScreen'
+
+it('renders without throwing', () => {
+    render(<OnboardingScreen />)
+})
