a62c3138bd8ef3e52e7b0cb36c863641a7aacd15
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-06-22 14:19:31 -0500

    created SearchBar

---
+++ meal_planning/src/inputs/SearchBar.spec.jsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react"
+import { SearchBar } from "./SearchBar"
+
+it("renders without throwing", () => {
+    render(<SearchBar />)
+})
---
+++ meal_planning/src/inputs/SearchBar.stories.jsx
@@ -0,0 +1,8 @@
+import { SearchBar } from "./SearchBar";
+
+export default {
+    title: "SearchBar",
+    component: SearchBar
+}
+
+export const Primary = () => <SearchBar />
