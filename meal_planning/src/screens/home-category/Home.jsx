90a9e19bbaf7d55b216beeb76dc93c4272952135
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-06-22 19:05:57 -0500

    renamed home to home-category

--- meal_planning/src/screens/home-category/Home.jsx
+++ meal_planning/src/screens/home-category/Home.jsx
@@ -1,10 +1,26 @@
 import { ScreenWithTitleAndNav } from "../common/ScreenWithTitleAndNav/ScreenWithTitleAndNav"
 import { SearchBar } from "../../inputs/SearchBar/SearchBar"
 import { TimeDietAllergies } from './TimeDietAllergies/TimeDietAllergies'
+import { RecipeRow } from "./RecipeRow/RecipeRow"
 
+/**
+ * @param {{
+ *  categories?: {
+ *      categoryTitle?: string,
+ *      recipes?: {
+ *          title?: string
+ *      }[]
+ *  }[]
+ * }} props 
+ */
 export function Home(props) {
     return <ScreenWithTitleAndNav title="Recipes" activeNavButton="RECIPES">
         <SearchBar />
         <TimeDietAllergies />
+        {
+            props.categories?.map(category => 
+                <RecipeRow rowTitle={category.categoryTitle} recipes={category.recipes} />
+            )
+        }
     </ScreenWithTitleAndNav>
 }
---
+++ meal_planning/src/screens/home-category/RecipeRow/RecipeRow.spec.jsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react'
+import { RecipeRow } from './RecipeRow'
+
+it('renders without throwing', () => {
+    render(<RecipeRow />)
+})
