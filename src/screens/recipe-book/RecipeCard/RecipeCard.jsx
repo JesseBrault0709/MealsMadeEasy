a39d5b725e3c11988bfdf496a3ec8b8d99378324
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-06-25 12:38:37 -0400

    default to DevRecipePicture

--- src/screens/recipe-book/RecipeCard/RecipeCard.jsx
+++ src/screens/recipe-book/RecipeCard/RecipeCard.jsx
@@ -1,3 +1,5 @@
+import DevRecipePicture from './assets/DevRecipePicture.png'
+
 import { Card } from "react-bootstrap"
 import { RecipeRating } from "../RecipeRating/RecipeRating"
 
@@ -10,7 +12,7 @@ import { RecipeRating } from "../RecipeRating/RecipeRating"
  */
 export function RecipeCard(props) {
     return <Card style={{ width: '90px' }}>
-        <Card.Img src={props.imgSrc} />
+        <Card.Img src={props.imgSrc ?? DevRecipePicture} />
         <Card.Title>{props.title}</Card.Title>
         {props.rating ? <RecipeRating rating={props.rating} /> : ''}
     </Card>
