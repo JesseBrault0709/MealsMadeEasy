d3f1e92c5b9aeba46e06617a337c18efa13ac6aa
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-06-25 12:37:46 -0400

    created RecipeBook spec

---
+++ src/screens/recipe-book/RecipeBook/RecipeBook.spec.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react'
+import { RecipeBook } from './RecipeBook'
+
+it('renders without throwing', () => {
+    render(<RecipeBook tags={[]} tabs={[]} recipes={[]} />)
+})
