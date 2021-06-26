c828a40d926b36e9528f900be85a522a43c985be
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-06-25 21:18:07 -0400

    fixed args

--- src/screens/recipe-book/TimeDietAllergies/TagChip/TagChip.stories.tsx
+++ src/screens/recipe-book/TimeDietAllergies/TagChip/TagChip.stories.tsx
@@ -10,18 +10,24 @@ const template: Story<TagChipProps> = args => <TagChip {...args} />
 
 export const TimeChip: Story<TagChipProps> = template.bind({})
 TimeChip.args = {
-    name: 'Time',
-    values: ['15 mins']
+    tag: {
+        name: 'Time',
+        values: ['15 mins']
+    }
 }
 
 export const DietChip: Story<TagChipProps> = template.bind({})
 DietChip.args = {
-    name: 'Diet',
-    values: ['Vegan', 'Vegetarian', 'Pescatarian']
+    tag: {
+        name: 'Diet',
+        values: ['Vegan', 'Vegetarian', 'Pescatarian']
+    }
 }
 
 export const AllergyChip: Story<TagChipProps> = template.bind({})
 AllergyChip.args = {
-    name: 'Allergies',
-    values: []
+    tag: {
+        name: 'Allergies',
+        values: []
+    }
 }
