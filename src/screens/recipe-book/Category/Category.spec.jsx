648dcf89ca5a72a253ba3bfed744fbb68b16ff72
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-06-25 21:18:46 -0400

    deleted home/category

--- src/screens/recipe-book/Category/Category.spec.jsx
+++
@@ -1,6 +0,0 @@
-import { render } from '@testing-library/react'
-import { Category } from './Category'
-
-it('renders without throwing', () => {
-    render(<Category />)
-})
--- src/screens/recipe-book/Home.spec.jsx
+++
@@ -1,6 +0,0 @@
-import { render } from '@testing-library/react'
-import { Home } from './Home'
-
-it('renders without throwing', () => {
-    render(<Home />)
-})
