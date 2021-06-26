fe3ece05324533e5b8577965b857edaeed76eb96
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-06-26 16:01:59 -0400

    stars now have keys

--- src/screens/recipe-book/RecipeRating/RecipeRating.jsx
+++ src/screens/recipe-book/RecipeRating/RecipeRating.jsx
@@ -1,4 +1,39 @@
-import Star from './assets/Star.png'
+import StarImg from './assets/Star.png'
+
+/**
+ * @param {any[]} arr 
+ * @param {(index: number) => any} filler
+ * @param {number} startIndex
+ * @param {number} endIndex
+ * 
+ * @returns {any[]}
+ */
+function fillUsingFiller(arr, filler, startIndex, endIndex) {
+    if (endIndex !== null && endIndex !== undefined && endIndex > arr.length) {
+        throw new Error(`endIndex cannot be greater than arr.length`)
+    }
+
+    if (startIndex < 0) {
+        throw new Error(`startIndex cannot be less than 0`)
+    }
+
+    const result = arr.slice()
+
+    for (let i = startIndex; i < (endIndex ?? arr.length); i++) {
+        result[i] = filler(i)
+    }
+
+    return result
+}
+
+/**
+ * @param {{
+ *  index: number
+ * }} props 
+ */
+function Star(props) {
+    return <img key={`star${props.index}`} src={StarImg} alt="star" />
+}
 
 /**
  * @param {{
@@ -7,6 +42,6 @@ import Star from './assets/Star.png'
  */
 export function RecipeRating(props) {
     return <div>
-        {new Array(5).fill(<img src={Star} alt="star" />, 0, props.rating)}
+        {fillUsingFiller(new Array(5), index => <Star index={index} />, 0, props.rating)}
     </div>
 }
