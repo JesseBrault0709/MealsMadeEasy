ac83da5345f76b9e3f76c41d13ca64943c294032
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2021-06-26 21:38:46 -0400

    hard-coded Tag-Chips instead of using props

--- src/screens/recipe-book/TimeDietAllergies/TimeDietAllergies.jsx
+++ src/screens/recipe-book/TimeDietAllergies/TimeDietAllergies.jsx
@@ -1,20 +1,16 @@
 import './TimeDietAllergies.css'
 import { TagChip } from "./TagChip/TagChip"
 
-/**
- * @param {{
- *  onFliterClick: () => void,
- *  tags: ReadonlyArray<{
- *     name: string,
- *     values: ReadonlyArray<string>
- *  }>
- * }} props
- */
-export function TimeDietAllergies(props) {
+export function TimeDietAllergies({ 
+    onFilterClick,
+    cookingTime,
+    diet,
+    intolerances
+}) {
     return <div className="time-diet-allergies">
-        <button type="button" onClick={props.onFilterClick}>[filter icon]</button>
-        {   
-            props.tags?.map(tag => <TagChip tag={tag} />)
-        }
+        <button type="button" onClick={onFilterClick}>[filter icon]</button>
+        <TagChip tag={{ name: "Cooking Time", values: [cookingTime] }} />
+        <TagChip tag={{ name: "Diet", values: [diet] }} />
+        <TagChip tag={{ name: "Allergies", values: intolerances }} />
     </div>
 }
