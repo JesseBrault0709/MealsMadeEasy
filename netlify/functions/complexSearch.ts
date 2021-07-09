23ee5fd93ac167d2b4f426335bac46934efa5bd1
Jesse Brault <jbrault@mac.com>
2021-07-09 17:23:10 -0400

    messing with env variable in func

--- netlify/functions/complexSearch.ts
+++ netlify/functions/complexSearch.ts
@@ -1,9 +1,10 @@
 import type { Handler } from '@netlify/functions'
 
 const handler: Handler = async (event, context) => {
+    const message = process.env.MESSAGE
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'Hello, Jesse!' })
+        body: JSON.stringify({ message })
     }
 }
 
