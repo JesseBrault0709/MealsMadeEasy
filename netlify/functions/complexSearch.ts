daee82c1e11c09b04e69c485e1207fd9bd25235f
Jesse Brault <jbrault@mac.com>
2021-07-09 17:30:23 -0400

    attempt to pull some recipes through netlify

--- netlify/functions/complexSearch.ts
+++ netlify/functions/complexSearch.ts
@@ -1,10 +1,30 @@
 import type { Handler } from '@netlify/functions'
 
+import axios from 'axios'
+
+const rapidAPIHost = "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+
+export function calcUrl(endPoint: string): string {
+    return `https://${rapidAPIHost}${endPoint}`
+}
+
 const handler: Handler = async (event, context) => {
-    const message = process.env.MESSAGE
+
+    const apiKey = process.env.SPOONACULAR_KEY
+
+    const results = await axios.get(
+        calcUrl('/recipes/complexSearch'),
+        {
+            headers: {
+                'x-rapidapi-key': apiKey,
+                'x-rapidapi-host': rapidAPIHost
+            }
+        }
+    )
+
     return {
         statusCode: 200,
-        body: JSON.stringify({ message })
+        body: JSON.stringify({ results })
     }
 }
 
