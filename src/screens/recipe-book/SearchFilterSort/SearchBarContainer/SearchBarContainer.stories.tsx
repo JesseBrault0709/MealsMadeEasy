60c5936a96d5c17b8ea3755da13f7f3cde75d137
Jesse Brault <jbrault@mac.com>
2021-08-03 10:33:27 -0400

    created SearchBarContainer CU-1anwjrc

---
+++ src/screens/recipe-book/SearchFilterSort/SearchBarContainer/SearchBarContainer.stories.tsx
@@ -0,0 +1,8 @@
+import { SearchBarContainer } from './SearchBarContainer'
+
+export default {
+    title: 'SearchFilterSort/SearchBarContainer',
+    component: SearchBarContainer
+}
+
+export const Primary = () => <SearchBarContainer />
---
+++ src/screens/recipe-book/SearchFilterSort/SearchBarContainer/SearchBarContainer.tsx
@@ -0,0 +1,9 @@
+import { SearchBar } from '../../../../inputs/SearchBar/SearchBar'
+
+export function SearchBarContainer() {
+    return (
+        <div className="search-bar-container">
+            <SearchBar />
+        </div>
+    )
+}
