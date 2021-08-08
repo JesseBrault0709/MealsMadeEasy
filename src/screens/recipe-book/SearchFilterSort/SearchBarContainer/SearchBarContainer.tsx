bcab28cadc5cb1d512fcb314518b4afa1f48a6b0
Jesse Brault <jbrault@mac.com>
2021-08-08 16:27:16 -0400

    SearchBarContainer uses foward ref

--- src/screens/recipe-book/SearchFilterSort/SearchBarContainer/SearchBarContainer.tsx
+++ src/screens/recipe-book/SearchFilterSort/SearchBarContainer/SearchBarContainer.tsx
@@ -1,9 +1,16 @@
-import { SearchBar } from '../../../../inputs/SearchBar/SearchBar'
+import { forwardRef } from 'react'
+import { PropsWithRef } from 'react'
+import {
+    SearchBar,
+    SearchBarProps
+} from '../../../../inputs/SearchBar/SearchBar'
 
-export function SearchBarContainer() {
-    return (
+export type SearchBarContainerProps = PropsWithRef<SearchBarProps>
+
+export const SearchBarContainer = forwardRef<HTMLInputElement, SearchBarProps>(
+    (props, ref) => (
         <div className="search-bar-container">
-            <SearchBar />
+            <SearchBar ref={ref} {...props} />
         </div>
     )
-}
+)
