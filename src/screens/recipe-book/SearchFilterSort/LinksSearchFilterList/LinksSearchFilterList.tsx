1541b0cd20badbfeae7c044c0bb794312d4ca5fc
Jesse Brault <jbrault@mac.com>
2021-08-08 12:55:43 -0400

    created LinksSearchFilterList

---
+++ src/screens/recipe-book/SearchFilterSort/LinksSearchFilterList/LinksSearchFilterList.tsx
@@ -0,0 +1,24 @@
+import {
+    SearchFilterListContainer,
+    SearchFilterListContainerProps
+} from '../SearchFilterListContainer/SearchFilterListContainer'
+import { SearchFilterListElement } from '../SearchFilterListElement/SearchFilterListElement'
+
+export type LinksSearchFilterListProps = {
+    title: string
+    links: ReadonlyArray<[link: string, onSelect: () => void]>
+    renderTitleButton: SearchFilterListContainerProps['renderTitleButton']
+}
+
+export function LinksSearchFilterList(props: LinksSearchFilterListProps) {
+    return (
+        <SearchFilterListContainer
+            title={props.title}
+            renderTitleButton={props.renderTitleButton}
+        >
+            {props.links.map(([link, onSelect]) => (
+                <SearchFilterListElement title={link} onClick={onSelect} />
+            ))}
+        </SearchFilterListContainer>
+    )
+}
