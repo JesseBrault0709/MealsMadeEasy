1bdb0229ebdd8f9f2d3a9bea119890588d8eb7ba
Jesse Brault <jbrault@mac.com>
2021-08-13 15:18:23 -0400

    documented complexSearch

--- src/client/complexSearch.ts
+++ src/client/complexSearch.ts
@@ -10,27 +10,105 @@ import type {
 } from './spoonacularTypes'
 import { calcUrl } from './util'
 
+/**
+ * The parameters to pass to getByComplexSearch()
+ *
+ * @see https://spoonacular.com/food-api/docs
+ */
 export type ComplexSearchParams = {
-    maxReadyTime?: number
-    diet?: SPDiet
-    intolerances?: ReadonlyArray<SPIntolerance>
-    type?: SPType
-    addRecipeInformation?: boolean
-    offset?: number
-    number?: number
+    /**
+     * The (natural language) recipe search query.
+     */
     query?: string
+
+    /**
+     * The maximum time in minutes it should take to
+     * prepare and cook the recipe.
+     *
+     * Known elsewhere as cookingTime.
+     * */
+    maxReadyTime?: number
+
+    /**
+     * The diet for which the recipes must be suitable.
+     */
+    diet?: SPDiet
+
+    /**
+     * All recipes returned must not contain ingredients
+     * that are not suitable for people with the
+     * intolerances entered.
+     *
+     * Known elsewhere as 'allergies'.
+     */
+    intolerances?: ReadonlyArray<SPIntolerance>
+
+    /**
+     * The type of recipe (eg. 'main course', etc.).
+     */
+    type?: SPType
+
+    /**
+     * The cuisine(s) of the recipes. One or more.
+     */
     cuisine?: ReadonlyArray<SPCuisine>
+
+    /**
+     * The strategy to sort recipes by.
+     */
     sort?: SPSortingOption
+
+    /**
+     * If set to true, you get more information
+     * about the recipes returned.
+     */
+    addRecipeInformation?: boolean
+
+    /**
+     * The number of results to skip (between 0 and 900).
+     */
+    offset?: number
+
+    /**
+     * The number of expected results (between 1 and 100).
+     */
+    number?: number
 }
 
+/**
+ * A utility type for converting all complexSearch params to strings.
+ */
 type SerializedComplexSearchParams = {
     [K in keyof ComplexSearchParams]: string
 }
 
+/**
+ * Converts the given number to a string suitable for a query param
+ * if not undefined, else returns undefined.
+ *
+ * @param n The number to convert
+ * @returns A string representing the number if number is not
+ *  undefined, else undefined.
+ */
 const serializeNumberParam = (n?: number): string | undefined => n?.toString()
 
+/**
+ * Converts the given boolean to a string suitable for a query param.
+ *
+ * @param b The boolean to convert
+ * @returns 'true' or 'false'
+ */
 const serializeBooleanParam = (b?: boolean): string => (b ? 'true' : 'false')
 
+/**
+ * Converts the given string to a string suitable as a query param; if
+ * undefined, returns undefined; if s.length === 0, returns undefined; else
+ * returns the original string.
+ *
+ * @param s The string to convert
+ * @returns The original string or undefined if the original is
+ *  undefined or s.length === 0.
+ */
 const serializeStringParam = (s?: string): string | undefined => {
     if (s === undefined || s.length === 0) {
         return undefined
@@ -39,6 +117,14 @@ const serializeStringParam = (s?: string): string | undefined => {
     }
 }
 
+/**
+ * Converts the given array of strings to a single string suitable
+ * as a query param for Spoonacular (i.e., a comma-separated list).
+ *
+ * @param arr The array of strings to convert.
+ * @returns A single string representing the array, or undefined
+ *  if the original arr was undefined or arr.length === 0.
+ */
 const serializeStringArrayParam = (
     arr?: ReadonlyArray<string>
 ): string | undefined => {
@@ -49,6 +135,16 @@ const serializeStringArrayParam = (
     }
 }
 
+/**
+ * Converts all of the given ComplexSearchParams to strings
+ * suitable for sending to Spoonacular via axios.
+ *
+ * @param params The params to convert.
+ * @returns A Record of string keys and string values (or an undefined
+ *  value if the original value was undefined) for each original param.
+ *
+ * @throws If a non-serializable value is contained within params.
+ */
 const serializeParams = (
     params: ComplexSearchParams
 ): SerializedComplexSearchParams => {
@@ -82,6 +178,9 @@ const serializeParams = (
     return result
 }
 
+/**
+ * The type of the data object returned from Spoonacular via axios.
+ */
 type Result = {
     results: ReadonlyArray<RecipeOverview>
     offset: number
@@ -89,6 +188,16 @@ type Result = {
     totalResults: number
 }
 
+/**
+ * Searches through Spoonacular's recipe database via
+ * its complexSearch endpoint.
+ *
+ * @param params The params to send to spoonacular via
+ *  the query string
+ * @returns A readonly array of RecipeOverviews.
+ *
+ * @see https://spoonacular.com/food-api/docs
+ */
 export async function getByComplexSearch(
     params: ComplexSearchParams
 ): Promise<ReadonlyArray<RecipeOverview>> {
