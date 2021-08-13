6c1daec45ad2010f5138f246b82d21f1cbebe199
Jesse Brault <jbrault@mac.com>
2021-08-13 10:37:13 -0400

    fixed param serialization for complexSearch

--- src/client/complexSearch.ts
+++ src/client/complexSearch.ts
@@ -23,6 +23,65 @@ export type ComplexSearchParams = {
     sort?: SPSortingOption
 }
 
+type SerializedComplexSearchParams = {
+    [K in keyof ComplexSearchParams]: string
+}
+
+const serializeNumberParam = (n?: number): string | undefined => n?.toString()
+
+const serializeBooleanParam = (b?: boolean): string => (b ? 'true' : 'false')
+
+const serializeStringParam = (s?: string): string | undefined => {
+    if (s === undefined || s.length === 0) {
+        return undefined
+    } else {
+        return s
+    }
+}
+
+const serializeStringArrayParam = (
+    arr?: ReadonlyArray<string>
+): string | undefined => {
+    if (arr === undefined || arr.length === 0) {
+        return undefined
+    } else {
+        return arr.join(',')
+    }
+}
+
+const serializeParams = (
+    params: ComplexSearchParams
+): SerializedComplexSearchParams => {
+    const result: Record<string, string | undefined> = {}
+    Object.entries(params).forEach(([key, value]) => {
+        switch (typeof value) {
+            case 'undefined':
+                return
+            case 'boolean':
+                result[key] = serializeBooleanParam(value)
+                return
+            case 'number':
+                result[key] = serializeNumberParam(value)
+                return
+            case 'string':
+                result[key] = serializeStringParam(value)
+                return
+            case 'object':
+                if (value instanceof Array) {
+                    result[key] = serializeStringArrayParam(value)
+                } else {
+                    throw new Error(
+                        `cannot serialize non-array object (given: ${value})`
+                    )
+                }
+                return
+            default:
+                throw new Error(`cannot serialize this value: ${value}`)
+        }
+    })
+    return result
+}
+
 type Result = {
     results: ReadonlyArray<RecipeOverview>
     offset: number
@@ -38,7 +97,9 @@ export async function getByComplexSearch(
     })
 
     return await axios
-        .get<Result>(calcUrl('/complexSearch'), { params })
+        .get<Result>(calcUrl('/complexSearch'), {
+            params: serializeParams(params)
+        })
         .then(response => {
             return response.data.results
         })
